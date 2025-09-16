7dd53bcc4d4a5fb64f740d9f5086a9a3b6636703
oliverhall45 <halloli@sheridancollege.ca>
2025-09-16 14:47:02 -0400

    Made an acceleration system
    made an acceleration system and also an unfinished deacceleration system

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -10,46 +10,52 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
     [Space(15)]
-    public float speed;
+    public float maxSpeed = 3f;
     public Vector3 playerPosition;
+    public float accelerationTime = 2f;
+    public float deaccelerationTime = -2f;
+    public float timePassed = 0f;
+
+    private Vector3 velocity = Vector3.zero;
     
 
     void Update()
     {
         PlayerMovement(); //constantly checks for button inputs in the method
-
-        playerPosition = transform.position;
-
-        if (transform.position.x <= -18.5f)
-        {
-            playerPosition.x = -18.5f;
-        }
-
     }
 
     public void PlayerMovement() //allows the player to move around with the arrow keys
     {
-        
+        float acceleration = maxSpeed / accelerationTime;
+        float deacceleration = maxSpeed / deaccelerationTime;
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+            velocity += acceleration * Time.deltaTime * Vector3.left;
+            timePassed += Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+            velocity += acceleration * Time.deltaTime * Vector3.right;
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+            velocity += acceleration * Time.deltaTime * Vector3.up;
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += new Vector3(0, -speed, 0) * Time.deltaTime;
+            velocity += acceleration * Time.deltaTime * Vector3.down;
         }
+
+        if (!Input.GetKey(KeyCode.LeftArrow) && (!Input.GetKey(KeyCode.RightArrow)) && (!Input.GetKey(KeyCode.UpArrow)) && (!Input.GetKey(KeyCode.DownArrow)))
+        {
+            velocity -= acceleration / Time.deltaTime * Vector3.zero;
+        }
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        transform.position += velocity * Time.deltaTime;
     }
 
 }

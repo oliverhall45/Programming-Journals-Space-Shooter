b0a7a093456a1a3f1e60d2f700aa842058384005
oliverhall45 <halloli@sheridancollege.ca>
2025-09-21 18:11:15 -0400

    Asteroid code
    Made an asteroid move randomly ever second or so

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -8,13 +8,39 @@ public class Asteroid : MonoBehaviour
     public float arrivalDistance;
     public float maxFloatDistance;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    private Vector2 targetPoint;
+    private bool hasTarget = false;
 
-    // Update is called once per frame
     void Update()
     {
+        AsteroidMovement();
+    }
+
+    public void AsteroidMovement()
+    {
+        Vector2 currentPosition = transform.position;
+
+        
+        if (!hasTarget || Vector2.Distance(currentPosition, targetPoint) <= arrivalDistance)
+        {
+            float randomX = Random.Range(-1f, 1f);
+            float randomY = Random.Range(-1f, 1f);
+            Vector2 randomDirection = new Vector2(randomX, randomY).normalized;
+
+            
+            if (randomDirection == Vector2.zero)
+            {
+                randomDirection = Vector2.up; //makes sure it doesnt move zero distance
+            }
+
+            //sets a new target for the object to move towards
+            targetPoint = currentPosition + randomDirection * maxFloatDistance;
+            hasTarget = true;
+        }
+
+        //move toward target
+        Vector2 direction = (targetPoint - currentPosition).normalized;
+        Vector2 newPosition = currentPosition + direction * moveSpeed * Time.deltaTime;
+        transform.position = newPosition;
     }
 }

fd15bc4d1d1f7feb9ed0336d532f06dd4a13ed21
oliverhall45 <halloli@sheridancollege.ca>
2025-09-23 14:30:57 -0400

    circle around player
    made a green circle follow the player

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -17,11 +17,15 @@ public class Player : MonoBehaviour
     public float timePassed = 0f;
 
     private Vector3 velocity = Vector3.zero;
-    
+
+    [Header("Circle drawer properties")]
+    public float radarRadius = 1f;
+    public int numberOfPoints = 6;
 
     void Update()
     {
         PlayerMovement(); //constantly checks for button inputs in the method
+        CircleDrawer(radarRadius, numberOfPoints);
     }
 
     public void PlayerMovement() //allows the player to move around with the arrow keys
@@ -77,6 +81,28 @@ public class Player : MonoBehaviour
         transform.position += velocity * Time.deltaTime;
 
        
+        
+    }
+
+    public void CircleDrawer(float radius, int numberOfPoints)
+    {
+        float angleStep = 360f / numberOfPoints;
+        float radians = angleStep * Mathf.Deg2Rad;
+
+        List<Vector3> points = new List<Vector3>();
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            float adjustment = radians * i;
+            Vector3 point = new Vector3(Mathf.Cos(radians + adjustment), Mathf.Sin(radians + adjustment)) * radians;
+
+            points.Add(point);
+        }
+
+        Vector3 center = transform.position;
+        for(int i = 0; i < points.Count - 1; i++)
+        {
+            Debug.DrawLine(center + points[i], center + points[i + 1], Color.green);
+        }
         
     }
 

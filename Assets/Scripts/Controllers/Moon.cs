857ee320c38e60a7159c443ce7c996dc03732e18
oliverhall45 <halloli@sheridancollege.ca>
2025-09-25 17:37:11 -0400

    Moon orbit
    made a moon able to orbit a planet at a specified speed and radius

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
+    public float radius;
+    public float orbitalSpeed;
+    private float angle;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +19,16 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(radius, orbitalSpeed, planetTransform);
+    }
 
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        angle += speed * Time.deltaTime;
+
+        float x = Mathf.Cos(angle) * radius;
+        float y = Mathf.Sin(angle) * radius;
+
+        transform.position = target.position + new Vector3(x, y, 0);
     }
 }
